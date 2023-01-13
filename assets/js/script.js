1164b546b14724e23c2776b469d6968577b103d9
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-13 00:18:46 -0700

    added hour slots

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
 var dayEl = $('#currentDay');
 var containerEl = $('#container');
 
-dayEl.text(moment().format('MMMM Do YYYY'));
+dayEl.text(moment().format('MMMM Do YYYY')); //date at header
+
+//hourly schedule for today
+
+var schedule = [[7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17], [18, 19, 20, 21, 22], [23, 24, 1, 2, 3, 4, 5, 6]];
+
+
+
+containerEl.append();
+
+
+// var schedule = $('<table />');
+// schedule[0].border = "12am";
+// containerEl.append(schedule);
